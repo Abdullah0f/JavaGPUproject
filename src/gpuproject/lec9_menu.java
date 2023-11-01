ea8dd674e2ed7a69e3788009f21ea7c98cb3d226
Abdullah Fahad <abdullah84f@gmail.com>
2023-11-01 11:23:43 +0300

    additions to lec9

--- src/gpuproject/lec9_menu.java
+++ src/gpuproject/lec9_menu.java
@@ -1,11 +1,14 @@
 package gpuproject;
 
+import java.awt.Color;
 import java.awt.event.*;
 
 import javax.swing.*;
-import javax.swing.JMenuBar;
 
 public class lec9_menu extends JFrame {
+    // add text
+    JTextArea text = new JTextArea("Hello World");
+
     // menubar
     JMenuBar menubar = new JMenuBar();
     // menu
@@ -18,21 +21,33 @@ public class lec9_menu extends JFrame {
     JMenuItem item2 = new JMenuItem("Open");
     JMenuItem item3 = new JMenuItem("Save");
     JMenuItem item4 = new JMenuItem("Exit");
-
+    JMenuItem print = new JMenuItem("print");
     // add as checkbox
     JCheckBoxMenuItem item5 = new JCheckBoxMenuItem("Dark Mode");
+
     // create submenu
     JMenu submenu = new JMenu("Color");
     // add submenu to menu
     JMenuItem item6 = new JMenuItem("Red");
     JMenuItem item7 = new JMenuItem("Blue");
     JMenuItem item8 = new JMenuItem("Green");
+    JMenuItem fontsize = new JMenuItem("Set Font Size");
+    JMenuItem fontsizeSlider = new JMenuItem("Set Font Size (slider)");
+    JMenuItem fontcolor = new JMenuItem("Font Color");
+
+    // create submenu
+
+    JMenu fontstyle = new JMenu("Font Style");
+    // create submenu items
+    // style
+    // make it radio button to select only one
+    JMenuItem fontstyle1 = new JRadioButtonMenuItem("Bold");
+    JMenuItem fontstyle2 = new JRadioButtonMenuItem("Italic");
 
     public lec9_menu() {
         setSize(400, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
-
+        addEvents();
         // add menu to menubar
         menubar.add(menu);
         menu.setMnemonic('F');
@@ -43,7 +58,48 @@ public class lec9_menu extends JFrame {
         menubar.add(menu4);
         menu4.setMnemonic('H');
 
-        // add action on item4 (exit) (using anonymous class)
+        // add menu items to menu
+        menu.add(item2);
+        menu.add(item1);
+        menu.add(item3);
+        menu.add(item4);
+        menu.add(print);
+        menu.add(item5);
+        // add to menu2
+        submenu.add(item6);
+        submenu.add(item7);
+        submenu.add(item8);
+
+        fontstyle.add(fontstyle1);
+        fontstyle.add(fontstyle2);
+
+        menu3.add(fontsize);
+        menu3.add(fontstyle);
+        menu3.add(fontcolor);
+        menu3.add(submenu);
+        menu3.add(fontsizeSlider);
+
+        // add menubar to frame
+        setJMenuBar(menubar);
+        text.setLineWrap(true);
+        add(new JScrollPane(text));
+        setVisible(true);
+
+    }
+
+    private void addEvents() {
+
+        fontsize.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String input = JOptionPane.showInputDialog("Enter font size:");
+                int size = Integer.parseInt(input);
+                getContentPane().setFont(new java.awt.Font("Arial",
+                        getContentPane().getFont().getStyle(), size));
+                text.setFont(new java.awt.Font("Arial",
+                        getContentPane().getFont().getStyle(), size));
+            }
+        });
+
         item4.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 System.exit(0);
@@ -53,33 +109,96 @@ public class lec9_menu extends JFrame {
         item6.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 getContentPane().setBackground(java.awt.Color.RED);
+                text.setBackground(java.awt.Color.RED);
             }
         });
         item7.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 getContentPane().setBackground(java.awt.Color.BLUE);
+                text.setBackground(java.awt.Color.BLUE);
             }
         });
         item8.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 getContentPane().setBackground(java.awt.Color.GREEN);
+                text.setBackground(java.awt.Color.GREEN);
             }
         });
 
-        // add menu items to menu
-        menu.add(item2);
-        menu.add(item1);
-        menu.add(item3);
-        menu.add(item4);
-        menu.add(item5);
-        // add to menu2
-        submenu.add(item6);
-        submenu.add(item7);
-        submenu.add(item8);
-        menu3.add(submenu);
+        print.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // print the text area content
+                try {
+                    text.print();
+                } catch (Exception e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
 
-        // add menubar to frame
-        setJMenuBar(menubar);
+        // open file
+        item2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                int result = fileChooser.showOpenDialog(null);
+            }
+        });
+
+        // add action to font size submenu
+
+        // // add action to font weight submenu
+        // font style is radio button so we need to add action to each item
+        fontstyle1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                getContentPane().setFont(new java.awt.Font("Arial",
+                        java.awt.Font.BOLD, getContentPane().getFont()
+                                .getSize()));
+                text.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD,
+                        getContentPane().getFont().getSize()));
+                fontstyle2.setSelected(false);
+            }
+        });
+        fontstyle2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                getContentPane().setFont(new java.awt.Font("Arial",
+                        java.awt.Font.PLAIN, getContentPane().getFont()
+                                .getSize()));
+                text.setFont(new java.awt.Font("Arial", java.awt.Font.ITALIC,
+                        getContentPane().getFont().getSize()));
+                fontstyle1.setSelected(false);
+            }
+        });
+
+        // // add action to font color submenu
+        fontcolor.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                Color color = JColorChooser.showDialog(null, "Choose a color",
+                        Color.BLACK);
+                getContentPane().setForeground(color);
+                text.setForeground(color);
+            }
+        });
+
+        fontsizeSlider.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // create pane that have a slider that changes font
+                // size
+                JPanel panel = new JPanel();
+                JSlider slider = new JSlider(0, 100);
+                slider.setMajorTickSpacing(10);
+                slider.setMinorTickSpacing(1);
+                slider.setPaintTicks(true);
+                slider.setPaintLabels(true);
+                panel.add(slider);
+                JOptionPane.showMessageDialog(null, panel);
+                getContentPane().setFont(new java.awt.Font("Arial",
+                        getContentPane().getFont().getStyle(), slider
+                                .getValue()));
+                text.setFont(new java.awt.Font("Arial",
+                        getContentPane().getFont().getStyle(), slider
+                                .getValue()));
+            }
+        });
     }
 
 }
